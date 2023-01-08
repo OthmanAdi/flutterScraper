0e18c2ffda4986f81c4d8dfb0bf42492623e29a6
Codingwithadi <github_me@outlook.de>
2023-01-08 12:04:35 +0100

    Hello scraper

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:web_scraper/web_scraper.dart';
+
+void main() => runApp(WebScraperApp());
+
+class WebScraperApp extends StatefulWidget {
+  @override
+  _WebScraperAppState createState() => _WebScraperAppState();
+}
+
+class _WebScraperAppState extends State<WebScraperApp> {
+  // initialize WebScraper by passing base url of website
+  final webScraper = WebScraper('https://webscraper.io');
+
+  // Response of getElement is always List<Map<String, dynamic>>
+  List<Map<String, dynamic>>? productNames;
+  late List<Map<String, dynamic>> productDescriptions;
+
+  void fetchProducts() async {
+    // Loads web page and downloads into local state of library
+    if (await webScraper
+        .loadWebPage('/test-sites/e-commerce/allinone/computers/laptops')) {
+      setState(() {
+        // getElement takes the address of html tag/element and attributes you want to scrap from website
+        // it will return the attributes in the same order passed
+        productNames = webScraper.getElement(
+            'div.thumbnail > div.caption > h4 > a.title', ['href', 'title']);
+        productDescriptions = webScraper.getElement(
+            'div.thumbnail > div.caption > p.description', ['class']);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Requesting to fetch before UI drawing starts
+    fetchProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
